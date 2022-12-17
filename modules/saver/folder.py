613b08ba174e239d6d6a1b4a5997d8fe581fbaac
salt <alotofcatz@gmail.com>
2022-12-17 04:34:46 -0800

    refactor folder module to make more sense

--- modules/saver/folder.py
+++ modules/saver/folder.py
@@ -1,8 +1,12 @@
 import os
 import shutil
+import json
 
 
 class Saver:
     def save(self, image, meta, args):
         os.makedirs(args[0], exist_ok=True)
         shutil.copyfileobj(image, open(args[0] + '/' + meta['image_name'] + meta['ext'], 'wb'))
+        if len(args) > 1 and args[1] == 'json':
+            with open(args[0] + '/' + meta['image_name'] + '.json', 'w') as f:
+                json.dump(meta, f)
