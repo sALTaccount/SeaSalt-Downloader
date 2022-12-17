8447289e2df692729fee21c23a4dc20161ede8bd
salt <alotofcatz@gmail.com>
2022-12-17 03:47:30 -0800

    Added readme.MD

--- modules/filters/tag_filter.py
+++ modules/filters/tag_filter.py
@@ -3,6 +3,6 @@ class Filt:
         if args:
             for arg in args:
                 if arg in meta['tags']:
-                    print('filtered out ', meta['image_name'])
+                    print('filtered out', meta['image_name'])
                     return None, None
         return image, meta
